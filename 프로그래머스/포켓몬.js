60b8ec589e6d59808ae86cb72b6bde9468bf96ce
YONG-TAE PARK <pyt4105@gmail.com>
2021-08-19 03:26:07 +0900

    :sparkles: 210819

--- 프로그래머스/포켓몬.js
+++ 프로그래머스/포켓몬.js
@@ -1,14 +1,13 @@
 function solution(nums) {
   
-  const set_nums = new Set (nums);
-  const setted_nums = [...set_nums]
+  const set_nums = [...new Set (nums)]
   let answer = 0;
   let check_nums = nums.length
 
-  if (setted_nums.length >= check_nums/2) {
+  if (set_nums.length >= check_nums/2) {
     answer = check_nums/2;
   }else {
-    answer = setted_nums.length;
+    answer = set_nums.length;
   }
 
   return answer;
